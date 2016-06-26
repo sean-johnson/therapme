854dc97fd0e4d1b01d9e5161e22dbcb7394c064b
Charlotte Smith <hello@seanjohnson.nz>
2016-06-26 17:11:50 +1200

    added sound bites, WIP animations

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,38 +1,44 @@
-var sounds = {
-	65 : 65, //A-key',
-	66 : 66, //B-key', 
-	67 : '', //C-key',
-	68 : '', //D-key',
-	69 : '', //E-key',
-	70 : '', //F-key',
-	71 : '', //G-key',
-	72 : '', //H-key',
-	73 : '', //I-key',
-	74 : '', //J-key',
-	75 : '', //K-key',
-	76 : '', //L-key',
-	77 : '', //M-key',
-	78 : '', //N-key',
-	79 : '', //O-key',
-	80 : '', //P-key',
-	81 : '', //Q-key',
-	82 : '', //R-key',
-	83 : '', //S-key',
-	84 : '', //T-key',
-	85 : '', //U-key',
-	86 : '', //V-key',
-	87 : '', //W-key',
-	88 : '', //X-key',
-	89 : '', //Y-key',
-	90 : '' //Z-key'
+var soundBites = {
+	65 : 65, //A-key - Mid
+	66 : 66, //B-key - Low
+	67 : 67, //C-key - Low
+	68 : 68, //D-key - Mid
+	69 : 69, //E-key
+	70 : 70, //F-key - Mid
+	71 : 71, //G-key - Mid
+	72 : 72, //H-key - Mid
+	73 : 73, //I-key 
+	74 : 74, //J-key - Mid
+	75 : 75, //K-key - Mid
+	76 : 76, //L-key - Mid
+	77 : 77, //M-key - Low
+	78 : 78, //N-key - Low
+	79 : 79, //O-key
+	80 : 80, //P-key
+	81 : 81, //Q-key
+	82 : 82, //R-key
+	83 : 83, //S-key - Mid
+	84 : 84, //T-key
+	85 : 85, //U-key
+	86 : 86, //V-key - Low
+	87 : 87, //W-key
+	88 : 88, //X-key - Low
+	89 : 89, //Y-key
+	90 : 90  //Z-key - Low
 }
 
 
 document.onkeydown = function(e) {
 
-	var soundId = sounds[e.keyCode]
+	var soundId = soundBites[e.keyCode]
+
+	function animateSound(key) {
+		console.log("Here is key press: ", key)
+		document.getElementById(soundId)
+	}
 
 	if (soundId) {
+		animateSound(soundId)
 		document.getElementById(soundId).innerHTML="<embed src='../sounds/main/" + soundId + ".wav' autostart=true loop=false volume=100 hidden=true>"
     	return true
     } else {
@@ -40,5 +46,6 @@ document.onkeydown = function(e) {
     	console.log("key not mapped : code is", e.keyCode);
     }	
 }
+
 
 
