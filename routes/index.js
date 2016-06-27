c9d0e48f745ba555c5b65cfec910db7fba6c9193
Charlotte Smith <hello@seanjohnson.nz>
2016-06-27 14:24:38 +1200

    adding loop

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'PME' });
+  res.render('index', { title: 'TherapMe' });
 });
 
 module.exports = router;
