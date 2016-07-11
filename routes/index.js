cdef1d398397bc3b6ac840737e587ba3923b39e2
Charlotte Smith <hello@seanjohnson.nz>
2016-07-11 13:04:46 +1200

    adding start of database

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'TherapMe' });
 });
 
+router.get('/mood-start', function(req, res, next) {
+  res.render('mood-start', { title: "How are you feeling?"});
+});
+
+router.get('/interface', function(req, res, next) {
+  res.render('interface', { title: "Play. Create."});
+});
+
 module.exports = router;
