63c96f437eabc632fd33f360724c600f1c74e420
Charlotte Smith <hello@seanjohnson.nz>
2016-07-14 06:19:11 +1200

    adding event-listener for mousemovements

--- client/components/InterfaceMusicPad.jsx
+++ client/components/InterfaceMusicPad.jsx
@@ -1,51 +1,110 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default () => {
-  return (
-    <div className="music-pad-container">
+import keydown from 'react-keydown'
 
-      <div className="interface">
-      	<div className="row music-pad-row">
-      		<div className="twelve columns hi-keys">
-      			<div className="music-pad-key" name="q" id="81-key"><div className="animation" id="81-animation"></div></div>
-      			<div className="music-pad-key" name="w" id="87-key"><div className="animation" id="87-animation"></div></div>
-      			<div className="music-pad-key" name="e" id="69-key"><div className="animation" id="69-animation"></div></div>
-      			<div className="music-pad-key" name="r" id="82-key"><div className="animation" id="82-animation"></div></div>
-      			<div className="music-pad-key" name="t" id="84-key"><div className="animation" id="84-animation"></div></div>
-      			<div className="music-pad-key" name="y" id="89-key"><div className="animation" id="89-animation"></div></div>
-      			<div className="music-pad-key" name="u" id="85-key"><div className="animation" id="85-animation"></div></div>
-      			<div className="music-pad-key" name="i" id="73-key"><div className="animation" id="73-animation"></div></div>
-      			<div className="music-pad-key" name="o" id="79-key"><div className="animation" id="79-animation"></div></div>
-      			<div className="music-pad-key" name="p" id="80-key"><div className="animation" id="80-animation"></div></div>
-      		</div>
-      	</div>
 
-      	<div className="row music-pad-row">
-      		<div className="twelve columns mid-keys">
-      			<div className="music-pad-key" name="a" id="65-key"><div className="animation" id="65-animation"></div></div>
-      			<div className="music-pad-key" name="s" id="83-key"><div className="animation" id="83-animation"></div></div>
-      			<div className="music-pad-key" name="d" id="68-key"><div className="animation" id="68-animation"></div></div>
-      			<div className="music-pad-key" name="f" id="70-key"><div className="animation" id="70-animation"></div></div>
-      			<div className="music-pad-key" name="g" id="71-key"><div className="animation" id="71-animation"></div></div>
-      			<div className="music-pad-key" name="h" id="72-key"><div className="animation" id="72-animation"></div></div>
-      			<div className="music-pad-key" name="j" id="74-key"><div className="animation" id="74-animation"></div></div>
-      			<div className="music-pad-key" name="k" id="75-key"><div className="animation" id="75-animation"></div></div>
-      			<div className="music-pad-key" name="l" id="76-key"><div className="animation" id="76-animation"></div></div>
-      		</div>
-      	</div>
+class InterfaceMusicPad extends Component {
 
-      	<div className="row music-pad-row">
-      		<div className="twelve columns low-keys">
-      			<div className="music-pad-key" name="z" id="90-key"><div className="animation" id="90-animation"></div></div>
-      			<div className="music-pad-key" name="x" id="88-key"><div className="animation" id="88-animation"></div></div>
-      			<div className="music-pad-key" name="c" id="67-key"><div className="animation" id="67-animation"></div></div>
-      			<div className="music-pad-key" name="v" id="86-key"><div className="animation" id="86-animation"></div></div>
-      			<div className="music-pad-key" name="b" id="66-key"><div className="animation" id="66-animation"></div></div>
-      			<div className="music-pad-key" name="n" id="78-key"><div className="animation" id="78-animation"></div></div>
-      			<div className="music-pad-key" name="m" id="77-key"><div className="animation" id="77-animation"></div></div>
-      		</div>
-      	</div>
+  constructor( props ) {
+    super( props );
+    this.state = {
+      key: '1'
+    };
+  }
+
+  componentWillReceiveProps( nextProps ) {
+    const { keydown: { event } } = nextProps;
+    if ( event ) {
+      this.setState( { key: event.which } )
+      var soundId = this.state.key
+      var soundKey = new Howl({
+        urls: ["../main/" + soundId + ".wav"]
+      })
+      if (this.state.key !== '') {
+        soundKey.play()
+        showColor(soundId)
+      }
+    }
+
+    function showColor(keyId) {
+
+        var idPressed = document.getElementById( keyId + "-animation")
+    		var max = 255
+    		var min = 1
+    		var x = Math.random() * (max - min) + min
+    		var y = Math.random() * (max - min) + min
+    		var z = Math.random() * (max - min) + min
+    		var colorGenerator = 'rgba(' + x + ',' + y + ',' + z + ',0.5)'
+
+    		var tl = new TimelineMax({paused:true})
+        tl.to(idPressed, .5, {
+    				backgroundColor: colorGenerator,
+    				width: "+=125px",
+    				height: "+=125px",
+    				borderRadius: "50%",
+    				ease:Elastic.easeOut,
+    			})
+    			.to(idPressed, .5, {
+    				width:10,
+    				height:10,
+    				backgroundColor: colorGenerator,
+    				ease:Strong.easeInOut})
+    			.restart()
+    }
+
+  }
+
+
+  render () {
+
+    return (
+      <div className="music-pad-container">
+
+        <div className="interface">
+        	<div className="row music-pad-row">
+        		<div className="twelve columns hi-keys">
+        			<div className="music-pad-key" name="q" id="81-key"><div className="animation" id="81-animation"></div></div>
+        			<div className="music-pad-key" name="w" id="87-key"><div className="animation" id="87-animation"></div></div>
+        			<div className="music-pad-key" name="e" id="69-key"><div className="animation" id="69-animation"></div></div>
+        			<div className="music-pad-key" name="r" id="82-key"><div className="animation" id="82-animation"></div></div>
+        			<div className="music-pad-key" name="t" id="84-key"><div className="animation" id="84-animation"></div></div>
+        			<div className="music-pad-key" name="y" id="89-key"><div className="animation" id="89-animation"></div></div>
+        			<div className="music-pad-key" name="u" id="85-key"><div className="animation" id="85-animation"></div></div>
+        			<div className="music-pad-key" name="i" id="73-key"><div className="animation" id="73-animation"></div></div>
+        			<div className="music-pad-key" name="o" id="79-key"><div className="animation" id="79-animation"></div></div>
+        			<div className="music-pad-key" name="p" id="80-key"><div className="animation" id="80-animation"></div></div>
+        		</div>
+        	</div>
+
+        	<div className="row music-pad-row">
+        		<div className="twelve columns mid-keys">
+        			<div className="music-pad-key" name="a" id="65-key"><div className="animation" id="65-animation"></div></div>
+        			<div className="music-pad-key" name="s" id="83-key"><div className="animation" id="83-animation"></div></div>
+        			<div className="music-pad-key" name="d" id="68-key"><div className="animation" id="68-animation"></div></div>
+        			<div className="music-pad-key" name="f" id="70-key"><div className="animation" id="70-animation"></div></div>
+        			<div className="music-pad-key" name="g" id="71-key"><div className="animation" id="71-animation"></div></div>
+        			<div className="music-pad-key" name="h" id="72-key"><div className="animation" id="72-animation"></div></div>
+        			<div className="music-pad-key" name="j" id="74-key"><div className="animation" id="74-animation"></div></div>
+        			<div className="music-pad-key" name="k" id="75-key"><div className="animation" id="75-animation"></div></div>
+        			<div className="music-pad-key" name="l" id="76-key"><div className="animation" id="76-animation"></div></div>
+        		</div>
+        	</div>
+
+        	<div className="row music-pad-row">
+        		<div className="twelve columns low-keys">
+        			<div className="music-pad-key" name="z" id="90-key"><div className="animation" id="90-animation"></div></div>
+        			<div className="music-pad-key" name="x" id="88-key"><div className="animation" id="88-animation"></div></div>
+        			<div className="music-pad-key" name="c" id="67-key"><div className="animation" id="67-animation"></div></div>
+        			<div className="music-pad-key" name="v" id="86-key"><div className="animation" id="86-animation"></div></div>
+        			<div className="music-pad-key" name="b" id="66-key"><div className="animation" id="66-animation"></div></div>
+        			<div className="music-pad-key" name="n" id="78-key"><div className="animation" id="78-animation"></div></div>
+        			<div className="music-pad-key" name="m" id="77-key"><div className="animation" id="77-animation"></div></div>
+        		</div>
+        	</div>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
+
+export default keydown(InterfaceMusicPad)
