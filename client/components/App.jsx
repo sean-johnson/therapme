c79c51aa8aa3bd7e6b7ac7d29e9674b4ec0ad172
Charlotte Smith <hello@seanjohnson.nz>
2016-07-14 03:49:20 +1200

    adding emoticons

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,14 +3,15 @@ import React from 'react'
 import ThingListContainer from '../containers/ThingListContainer'
 import ThingDetailContainer from '../containers/ThingDetailContainer'
 import Interface from './Interface'
+import SessionStart from './SessionStart'
 
 export default () => {
   return (
     <div>
-      <Interface />
+      <SessionStart />
     </div>
   )
 }
- 
+
 // <ThingListContainer />
 // <ThingDetailContainer />
